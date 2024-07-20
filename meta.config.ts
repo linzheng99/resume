d5ac6be17833dc59e7d8bbd6b0e21d1a68cf9cce
linzheng <alidlg9045@126.com>
2024-07-20 21:32:11 +0800

    feat: 抽离配置

---
+++ meta.config.ts
@@ -0,0 +1,84 @@
+export default {
+  // 名字
+  name: 'Lin Zheng',
+  // 联系方式
+  connection: '( wx: linzheng-99 )',
+  links: [
+    {
+      icon: 'location',
+      value: 'WenZhou'
+    },
+    {
+      icon: 'weixin',
+      value: 'linzheng-99'
+    },
+    {
+      icon: 'email',
+      value: 'linzheng9045@gmail.com'
+    },
+    {
+      icon: 'github',
+      value: 'GitHub',
+      url: 'https://github.com/linzheng99'
+    }
+  ],
+  // 个人介绍
+  introduce: `
+      这里写个人评价 这里写个人评价 这里写个人评价 这里写个人评价
+      这里写个人评价 这里写个人评价 这里写个人评价 这里写个人评价
+      这里写个人评价 这里写个人评价 这里写个人评价 这里写个人评价
+  `,
+  // 公司
+  companies: [
+    {
+      name: '融威科技有限公司',
+      time: '2021年6月 - 2024年7月',
+      projects: [
+        {
+          name: '物联网管理后台(PC)',
+          details: '基于 star 面试法来写 - 背景 - 任务 - 行动 - 结果'
+        },
+        {
+          name: '物联网管理后台(H5)',
+          details: '写2 - 3 个核心项目就足够了, 能体现出你的价值的'
+        },
+        {
+          name: '数字龙舟(小程序)',
+          details: '写2 - 3 个核心项目就足够了, 能体现出你的价值的'
+        }
+      ]
+    },
+    {
+      name: '杭州促极科技有限公司',
+      time: '2020年9月 - 2021年3月',
+      projects: [
+        {
+          name: '国际电商平台',
+          details: '公司使用 React 及 Angular 框架,公司主要项目经营国内外电商pc平台，熟悉电商订单区域的业务逻辑,参与过功能开发，BUG修复，功能优化，页面美化等'
+        },
+      ]
+    },
+  ],
+  // 个人开源项目
+  projects: [
+    {
+      name: 'tob后台管理项目',
+      details: 'XXXXXX'
+    },
+    {
+      name: '地图组件库',
+      details: 'XXXXXX'
+    }
+  ],
+  // 技能
+  skills: ['HTML', 'CSS', 'JavaScript', 'TypeScript', 'Vue3/2', 'React', 'Vite', 'Webpack', 'Unocss/Tailwind Css', 'Nodejs'],
+  // 教育经历
+  educations: [
+    {
+      time: '2017年 ~ 2020年',
+      school: '浙江长征职业技术学院',
+      profession: '计算机应用技术',
+      lv: '专科'
+    }
+  ]
+}
